94f2fece4345c42e8cd5e0f314d288ea818121c4
adarshdubey-alphabotz <adarshdubey241010@gmail.com>
2025-10-29 13:04:30 +0530

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,7 +1,7 @@
 # bot_config.py
 API_ID = 7828653
 API_HASH = "8a81215989c379cff068a88aa7b24f96"
-BOT_TOKEN = "8237225312:AAFXoH40-wDE_8d_5iaDlj1iJuIe-hEmkEU"
+BOT_TOKEN = "8285049707:AAGQcdc1FNzstLTO5LmSy17pnoNyG4Hgce0"
 
 DEMO_TEXT = {
     "split": "Demo: Send video, choose split type, supply split times like 1:15 (mm:ss) or 0:05. Hit /done to finish multiple splits.",
